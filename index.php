62f7dfced9f0d440301f8c9e21747640b83c429b
devesh gour <deveshgour98@gmail.com>
2022-12-23 00:05:13 +0530

    update html

--- index.php
+++ index.php
@@ -1,25 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Admin || Register</title>
-    <?php include('include/head-link.php'); ?>
-   
-</head>
-
-<body>
-    <?php include('include/header.php'); ?>
-    <main class="mainContent authPage">
-        
-    
-    </main>
-    <?php include('include/footer.php') ?>
-    <?php include('include/footer-scripts.php') ?>
-
-
-
- 
-</body>
-
-</html>
+<div class="authPage_bg">
+    <div class="authPage_inner">
+        <div class="authPage_txt">
+            <span>DON'T HAVE AN ACCOUNT?</span>
+            <h3>Create new account</h3>
+            <form>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group floting">
+                            <input type="text" class="form-control form-control-border" id="fname" placeholder="" value="John" required>
+                            <label class="form-control-placeholder" for="fname">First Name</label>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group floting">
+                            <input type="text" class="form-control form-control-border" id="lname" placeholder="" value="Doe" required>
+                            <label class="form-control-placeholder" for="lname">Last Name</label>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="form-group floting">
+                            <input type="text" class="form-control form-control-border" id="email" placeholder="" value="jdoe@gmail.com" required>
+                            <label class="form-control-placeholder" for="email">Email</label>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="form-group floting">
+                            <input type="text" class="form-control form-control-border" id="password" placeholder="" value="text@123" required>
+                            <label class="form-control-placeholder" for="password">Password</label>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="form-group custom-checkbox mb-0">
+                            <input type="checkbox" class="custom-control-input" id="exampleCheck1">
+                            <label class="custom-control-label mb-0" for="exampleCheck1">I agreed on <a href="javascript:void(0);"> Terms & Conditions.</a> </label>
+                        </div>
+                    </div>
+                    <a href="javascript:void(0);" class="btn btn-secondary">CREATE ACCOUNT</a>
+                </div>
+            </form>
+            <p>Already A Member? <a href="javascript:void(0);">LOGIN</a> </p>
+        </div>
+    </div>
+</div>
